85319ba30e6caebe44fdfffbf3e669447252b36a
Redwan Alloush <redwaniscool@gmail.com>
2024-09-20 02:25:35 +0300

    Update extension.js

--- src/extension.js
+++ src/extension.js
@@ -5,49 +5,47 @@ import * as AppDisplay from 'resource:///org/gnome/shell/ui/appDisplay.js';
 let originalGetInitialResultSet = null;
 
 
-const TrancodeRusToEngDict = {
-    'а': 'f',
-    'б': ',',
-    'в': 'd',
-    'г': 'u',
-    'д': 'l',
-    'е': 't',
-    'ё': '`',
-    'ж': ';',
-    'з': 'p',
-    'и': 'b',
-    'й': 'q',
-    'к': 'r',
-    'л': 'k',
-    'м': 'v',
-    'н': 'y',
-    'о': 'j',
-    'п': 'g',
-    'р': 'h',
-    'с': 'c',
-    'т': 'n',
-    'у': 'e',
-    'ы': 's',
-    'і': 's',
-    'х': '[',
-    'ф': 'a',
-    'ц': 'w',
-    'ч': 'x',
-    'ш': 'i',
-    'щ': 'o',
-    'ь': 'm',
-    'ъ': ']',
-    'ї': ']',
-    'э': '',
-    'є': '',
-    'ю': '.',
-    'я': 'z'
+const TrancodeAraToEngDict = {
+    'q': 'ض',
+    'w': 'ص',
+    'e': 'ث',
+    'r': 'ق',
+    't': 'ف',
+    'y': 'غ',
+    'u': 'ع',
+    'i': 'ه',
+    'o': 'خ',
+    'p': 'ح',
+    '[': 'ج',
+    ']': 'د',
+    'a': 'ش',
+    's': 'س',
+    'd': 'ي',
+    'f': 'ب',
+    'g': 'ل',
+    'h': 'ا',
+    'j': 'ت',
+    'k': 'ن',
+    'l': 'م',
+    ';': 'ك',
+    '\'': 'ط',
+    'z': 'ئ',
+    'x': 'ء',
+    'c': 'ؤ',
+    'v': 'ر',
+    'b': 'ﻻ',
+    'n': 'ى',
+    'm': 'ة',
+    ',': 'و',
+    '.': 'ز',
+    '/': 'ظ',
+    '`': 'ذ'
 }
 
 const TrancodeEngToRusDict = {};
 
 function generateInvertedDict(sourceDict, destDict) {
-    for(let sourceindex in sourceDict)  {
+    for (let sourceindex in sourceDict) {
         destDict[sourceDict[sourceindex]] = sourceindex;
     }
 }
@@ -69,16 +67,16 @@ function transcode(source, dict) {
 function getResultSet(terms) {
     let query = terms.join(' ');
     let groups = Gio.DesktopAppInfo.search(query);
-    groups = groups.concat(Gio.DesktopAppInfo.search(transcode(query, TrancodeRusToEngDict)));
+    groups = groups.concat(Gio.DesktopAppInfo.search(transcode(query, TrancodeAraToEngDict)));
     groups = groups.concat(Gio.DesktopAppInfo.search(transcode(query, TrancodeEngToRusDict)));
     let usage = Shell.AppUsage.get_default();
     let results = [];
-    groups.forEach(function(group) {
-        group = group.filter(function(appID) {
+    groups.forEach(function (group) {
+        group = group.filter(function (appID) {
             let app = Gio.DesktopAppInfo.new(appID);
             return app && app.should_show();
         });
-        results = results.concat(group.sort(function(a, b) {
+        results = results.concat(group.sort(function (a, b) {
             return usage.compare(a, b);
         }));
     });
@@ -87,7 +85,7 @@ function getResultSet(terms) {
 
 export default class TranscodeAppSearchExtension {
     enable() {
-        generateInvertedDict(TrancodeRusToEngDict, TrancodeEngToRusDict);
+        generateInvertedDict(TrancodeAraToEngDict, TrancodeEngToRusDict);
         originalGetInitialResultSet = AppDisplay.AppSearchProvider.prototype.getInitialResultSet;
         AppDisplay.AppSearchProvider.prototype.getInitialResultSet = getResultSet;
     }
